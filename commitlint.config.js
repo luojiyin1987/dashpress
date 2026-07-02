e38c8a9e97f753d7b24baa9542f9b5efd854fd93
luojiyin <luojiyin@hotmail.com>
2026-07-02 10:39:20 +0800

    ci(commitlint): switch from emoji to conventional commits

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,23 +1,3 @@
 module.exports = {
-  extends: ["git-commit-emoji"],
-  rules: {
-    "type-enum": [
-      2,
-      "always",
-      [
-        "🎉 init",
-        "✨ feat",
-        "🐛 fix",
-        "📝 docs",
-        "💎 style",
-        "♻️ refactor",
-        "📈 perf",
-        "🧪 test",
-        "🏗️ build",
-        "📦 ci",
-        "🧹 chore",
-        "↩ revert",
-      ],
-    ],
-  },
+  extends: ["@commitlint/config-conventional"],
 };
